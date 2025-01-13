81f46458592d1524296d4a86c3dde9e48d04b2d0
khatrisarada <saradakhattri07@gmail.com>
2025-01-13 19:02:10 +0545

    [Add] convert character in a string to uppercase.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // What are some commonly used methods for manipulating strings in JavaScript? Provide examples demonstrating their usage.
 
+
+//String Methods
+
+const str = "Hello, JavaScript!";
+console.log(str.toUpperCase()); 
+console.log(str.toLowerCase()); 
+console.log(str.includes("JavaScript")); 
+console.log(str.split(", ")); 
+console.log(str.replace("JavaScript", "World")); 
+
+
+
 console.log("=>Concatination");
 let str1 ="Good";
 let str2 ="Morning";
@@ -11,3 +23,7 @@ console.log("=>String Length");
 let text ="Sarada";
 let Length= text.length;
 console.log(Length);
+
+console.log("=>toUpperCase")
+const upper = "sarada";
+console.log(upper.toUpperCase());
