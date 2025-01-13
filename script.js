74d813983e1547746e8959f137341630290055fa
khatrisarada <saradakhattri07@gmail.com>
2025-01-13 18:50:28 +0545

    [add] concatination and string Lenght

---
+++ script.js
@@ -0,0 +1,13 @@
+// What are some commonly used methods for manipulating strings in JavaScript? Provide examples demonstrating their usage.
+
+console.log("=>Concatination");
+let str1 ="Good";
+let str2 ="Morning";
+let result = str1.concat(" ",str2);
+console.log(result);
+
+
+console.log("=>String Length");
+let text ="Sarada";
+let Length= text.length;
+console.log(Length);
