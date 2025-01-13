bb392bfae0406d3a23cf402d87c471bf40bc8fc8
khatrisarada <saradakhattri07@gmail.com>
2025-01-13 19:06:17 +0545

    [Add] convert all characters in a string to lowercase

--- script.js
+++ script.js
@@ -11,19 +11,28 @@ console.log(str.split(", "));
 console.log(str.replace("JavaScript", "World")); 
 
 
-
+//Concatenation
 console.log("=>Concatination");
 let str1 ="Good";
 let str2 ="Morning";
 let result = str1.concat(" ",str2);
 console.log(result);
 
-
+//String Length
 console.log("=>String Length");
 let text ="Sarada";
 let Length= text.length;
 console.log(Length);
 
+
+
+//toUpperCase
 console.log("=>toUpperCase")
 const upper = "sarada";
 console.log(upper.toUpperCase());
+
+
+//toLowerCase
+console.log("=>toUpperCase")
+const lower = "SARADA";
+console.log(lower.toLowerCase());
